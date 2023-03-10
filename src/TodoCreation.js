d27bfab06b3ad17e75daf13c1ca4fdf9a801889d
saskyaF <92300218+saskyaF@users.noreply.github.com>
2023-03-10 14:36:28 +0100

    commit V1 React

---
+++ src/TodoCreation.js
@@ -0,0 +1,12 @@
+
+
+
+export default function TodoCreation() {
+  return (
+    <div className="bg-slate-900 flex justify-center items-center">
+     <input className="px-4 border-5 border-y-8 border-amber-50 rounded-2xl"></input>
+     <button  className="text-gray-50 p-2 m-5 rounded-2xl border-2">+Add</button>
+    </div>
+  );
+}
+
